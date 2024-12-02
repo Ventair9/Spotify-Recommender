14a22b7dc2466dc5fdc93f5dc1595627cd8e9b8f
Simon Tschammer <tschammer.simon@gmail.com>
2024-12-02 18:14:41 +0100

    grrrrrrrrrrr
    g

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
-#lets make this one a little bit more serious.
-from flask import Flask, session, redirect, request
+from flask import Flask, session, redirect, request, jsonify
 import requests
 import base64
 import urllib.parse
 import os
-
-#token logic
+import secrets
 
 app = Flask(__name__)
-
-app.secret_key = os.getenv("SECRET_KEY")
 
 class SpotifyOAuth2:
     def __init__(self):
         self.app = app
-        self.secret_key = os.getenv("CLIENT_SECRET")
+        self.client_secret = os.getenv("CLIENT_SECRET")
         self.client_id = os.getenv("CLIENT_ID")
-
+        self.redirect_uri = os.getenv("REDIRECT_URI")
+        app.secret_key = os.getenv("SECRET_KEY")
 
     def login(self):
-        state = self.token_urlsafe(16)
+        state = secrets.token_urlsafe(16)
         scope = "user-read-private user-read-email user-library-read playlist-modify-public playlist-modify-private"
         query_parameters = {
             "client_id": self.client_id,
@@ -57,7 +54,8 @@ class SpotifyOAuth2:
         session["access_token"] = json_response["access_token"]
         return redirect("/token")
     
-    def search_track(self):
+    # with user input for track_name, search for track and return track name
+    def search_track(self, track_name):
         token = session.get("access_token")
         endpoint_url = "https://api.spotify.com/v1/search"
         request_parameters = {
@@ -68,17 +66,83 @@ class SpotifyOAuth2:
         headers = {"Authorization": "Bearer " + token}
 
         response = requests.get(endpoint_url, params=request_parameters, headers=headers)
-        return response.json()["tracks"]["items"][0]
+        data = response.json()
 
-        """
-        wtf do i actually want to do? search for track, use track that was searched for to get recommendations
-        recommendations required parameters: seed_artist, seed_genres, seed_tracks
-        seed_artists = spotify id for artists
-        seed_genres = genres(from genre seeds)
-        seed_tracks = spotify id for track
-        search for track -> response: tracks object -> items array -> id for the track
-                            response: artist object -> items array -> id for the artist
-        get audio features for searched track, requires: spotify ID for the track
-        response = audio features
-        save audio features of track, use them in the get recommendations request to find good songs
-        """
+        track = data["tracks"]["items"][0]
+        return {
+            "track_id": track["id"],
+            "artist_id": track["artists"][0]["id"],
+            "track_name": track["name"],
+            "artist_name": track["artists"][0]["name"]
+        }
+
+    def get_audio_features(self, track_id):
+        token = session.get("access_token")
+        url = f"https://api.spotify.com/v1/audio-features/{track_id}"
+        headers = {"Authorization": "Bearer " + token}
+
+        response = requests.get(url, headers=headers)
+        return response.json()
+
+    def get_recommendations(self, seed_tracks, seed_artists):
+        token = session.get("access_token")
+        url = "https://api.spotify.com/v1/recommendations"
+        parameters = {
+            "seed_tracks": ",".join(seed_tracks),
+            "seed_artists": ",".join(seed_artists),
+            "limit": 10
+        }
+        headers = {"Authorization": "Bearer " + token}
+
+        response = requests.get(url, params=parameters, headers=headers)
+        return response.json()["tracks"]
+        
+spotify = SpotifyOAuth2()
+
+@app.route('/')
+def home():
+    return '''
+        <form action="/search" method="post">
+            <label for="track_name">Enter Song Name:</label>
+            <input type="text" id="track_name" name="track_name" required>
+            <button type="submit">Search</button>
+        </form>
+    '''
+
+@app.route("/login")
+def login():
+    return spotify.login()
+
+@app.route("/callback")
+def callback():
+    return spotify.callback()
+
+@app.route("/search", methods=["POST"])
+def search():
+    track_name = request.form.get("track_name")
+    track_info = spotify.search_track(track_name)
+    track_id = track_info.get("track_id")
+    print(f"Track ID extracted: {track_id}")
+    print(track_info)
+    audio_features = spotify.get_audio_features(track_id)
+    return jsonify({
+        "track_info": track_info,
+        "audio_features": audio_features
+    })
+
+@app.route("/audio_features")
+def audio_features():
+    track_id  = request.args.get("track_id")
+    print("Track ID:", track_id)
+    features = spotify.get_audio_features(track_id)
+    return jsonify(features)
+
+@app.route("/recommendations")
+def recommendations():
+    seed_tracks = request.args.get("seed_tracks").split(",")
+    seed_artists = request.args.get("seed_artists").split(",")
+    recommendations = spotify.get_recommendations(seed_tracks, seed_artists)
+    return jsonify(recommendations)
+
+if __name__ == "__main__":
+    app.run(port=8888, debug=True)
